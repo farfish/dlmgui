2df1ae168aa84ee93a400728cf5d92b450e4ae30
Jamie Lentin <jm@lentin.co.uk>
2019-11-22 10:38:30 +0000

    dlmtool_glossary: add dlmtool_slot_info_fn
    Scrapes the help text for individual data slots

--- dlmtool_glossary.R
+++ dlmtool_glossary.R
@@ -1,14 +1,21 @@
 library(xml2)
 library(DLMtool)
 
-dlmtool_method_info <- function (method_code) {
-    help_file <- tryCatch(utils:::.getHelpFile(help(method_code, 'DLMtool')), error = function (e) character())
+xml_help_file <- function(topic, pkg_ref) {
+    # NB: (topic), (pkg_ref) is magical, see help help
+    help_file <- tryCatch(utils:::.getHelpFile(help((topic), (pkg_ref))), error = function (e) character())
     if (length(help_file) == 0) {
+        return(NULL)
+    }
+    return(xml2::read_html(paste(capture.output(tools:::Rd2HTML(help_file)), collapse = "\n")))
+}
+
+dlmtool_method_info <- function (method_code) {
+    doc <- xml_help_file(method_code, 'DLMtool')
+
+    if (is.null(doc)) {
         return(c(code = method_code, name = '', description = ''))
     }
-
-    doc <- xml2::read_html(paste(capture.output(tools:::Rd2HTML(help_file)), collapse = "\n"))
-    
     return(c(
         code = method_code,
         name = xml2::xml_text(xml2::xml_find_first(doc, '/html/body/h2')),
@@ -28,4 +35,13 @@ dlmtool_help_link <- function (method_codes) {
     method_codes <- as.character(method_codes)  # NB: Resolve any factors first
     urls <- dlmtool_help_url(method_codes)
     ifelse(urls == "", method_codes, paste0('<a href="', urls, '" target="_blank">', method_codes, '</a>'))
+}
+
+dlmtool_slot_info_fn <- function () {
+    doc <- xml_help_file('Data-class', 'DLMtool')
+
+    return(function (slot_name) {
+        xml2::xml_text(xml2::xml_find_first(doc, paste0(
+            "/html/body/dl/dt[code/text() = '", slot_name, "']/following-sibling::dd")))
+    })
 }
